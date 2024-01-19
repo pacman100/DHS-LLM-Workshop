c2fc1d2075fc181f61f958cef6ced83a494c2341
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2024-01-19 17:14:59 +0530

    Update utils.py

--- chat_assistant/training/utils.py
+++ chat_assistant/training/utils.py
@@ -123,6 +123,7 @@ def create_and_prepare_model(args):
         device_map=device_map,
         trust_remote_code=True,
         attn_implementation="flash_attention_2" if args.use_flash_attn else "eager",
+        torch_dtype=torch.flaot32,
     )
 
     peft_config = None
